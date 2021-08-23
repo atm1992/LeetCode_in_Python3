70575dde52f9c032c9e52b86b66cad2089d0b0cc
Liu Quan 刘泉 <liuquan@wisers.com>
2021-08-24 01:03:42 +0800

    update

--- All_Solutions/a12_integer-to-roman.py
+++ All_Solutions/a12_integer-to-roman.py
@@ -49,4 +49,32 @@ Constraints:
 
 class Solution:
     def intToRoman(self, num: int) -> str:
-        pass
+        """模拟罗马数字的唯一表示法"""
+        mappings = [(1000, 'M'), (900, 'CM'), (500, 'D'), (400, 'CD'), (100, 'C'), (90, 'XC'), (50, 'L'), (40, 'XL'),
+                    (10, 'X'), (9, 'IX'), (5, 'V'), (4, 'IV'), (1, 'I')]
+        res = []
+        for value, symbol in mappings:
+            while num >= value:
+                num -= value
+                res.append(symbol)
+            if num == 0:
+                break
+        return ''.join(res)
+
+    def intToRoman_2(self, num: int) -> str:
+        """硬编码数字。千位数字只能由 M 表示；百位数字只能由 C、CD、D、CM 表示；十位数字只能由 X、XL、L、XC 表示；个位数字只能由 I、IV、V、IX 表示。"""
+        # 因为 num <= 3999，所以千位数字只需编码到3即可。若某一位置上的数字为0，则用空字符串表示
+        THOUSANDS = ['', 'M', 'MM', 'MMM']
+        HUNDREDS = ['', 'C', 'CC', 'CCC', 'CD', 'D', 'DC', 'DCC', 'DCCC', 'CM']
+        TENS = ['', 'X', 'XX', 'XXX', 'XL', 'L', 'LX', 'LXX', 'LXXX', 'XC']
+        ONES = ['', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX']
+
+        thousands_digit = num // 1000
+        hundreds_digit = num % 1000 // 100
+        tens_digit = num % 100 // 10
+        ones_digit = num % 10
+        return ''.join([THOUSANDS[thousands_digit], HUNDREDS[hundreds_digit], TENS[tens_digit], ONES[ones_digit]])
+
+
+if __name__ == '__main__':
+    print(Solution().intToRoman_2(3999))
