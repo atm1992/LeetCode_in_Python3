8364fd8a21aa84da8be3c7865c16ef0df2b7d54c
Liu Quan 刘泉 <liuquan@wisers.com>
2021-10-23 16:07:30 +0800

    update

--- All_Solutions/a79_word-search.py
+++ All_Solutions/a79_word-search.py
@@ -32,7 +32,39 @@ from typing import List
 
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
+        """回溯"""
         m, n = len(board), len(board[0])
         k = len(word)
         if m * n < k:
             return False
+        directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+
+        def dfs(row: int, col: int, idx: int):
+            if board[row][col] != word[idx]:
+                return False
+            if idx == k - 1:
+                return True
+
+            # 使用 # 表示该位置已访问过
+            store_val = board[row][col]
+            board[row][col] = '#'
+            flag = False
+            for di, dj in directions:
+                new_row, new_col = row + di, col + dj
+                if 0 <= new_row < m and 0 <= new_col < n and board[new_row][new_col] != '#' and dfs(new_row, new_col,
+                                                                                                    idx + 1):
+                    flag = True
+                    break
+            board[row][col] = store_val
+            return flag
+
+        # 依次将board中的每个单元格作为起始搜索位置
+        for i in range(m):
+            for j in range(n):
+                if dfs(i, j, 0):
+                    return True
+        return False
+
+
+if __name__ == '__main__':
+    print(Solution().exist(board=[["A", "B", "C", "E"], ["S", "F", "C", "S"], ["A", "D", "E", "E"]], word="ABCCED"))
