32e28fbdf577742f74362e0fc214cf7448e6eaff
Liu Quan 刘泉 <liuquan@wisers.com>
2021-11-10 00:28:55 +0800

    update

--- All_Solutions/a97_interleaving-string.py
+++ All_Solutions/a97_interleaving-string.py
@@ -35,6 +35,37 @@ Follow up: Could you solve it using only O(s2.length) additional memory space?
 
 class Solution:
     def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
+        """动态规划。
+        s = s1 + s2 + ... + sn
+        t = t1 + t2 + ... + tm
+        |n - m| <= 1
+        注意：不要误解题意，题目并没有要求len(s1) == len(s2) == len(sn)，也没有要求len(s1) == len(t1)，
+        因此，|n - m| <= 1 是一个必然满足的条件。如果s3从下标i到下标j的字符都是来自s1，那么完全可将s1中下标i到下标j的字符作为一个整体(子字符串)，
+        而下标i之前以及下标j之后的字符串必然来自s2，因为如果下标j后面的字符依旧来自s1，那么完全可将这些字符跟前面的下标i到下标j合并为一个子字符串，
+        直到某个下标之后的字符是来自s2。
+        题解：假设 dp[i][j] 表示s3中的前i+j个字符能否由s1中的前i个字符 和 s2中的前j个字符交错而来。字符串的下标从0开始，前0个字符表示空字符串，
+        由Example 3可知，dp[0][0] = True。dp[i][j] 可以由 dp[i-1][j] 或 dp[i][j-1] 状态转移而来。
+        若s3[i+j-1]==s1[i-1] 且 dp[i-1][j] 为True，则dp[i][j]也为True；若s3[i+j-1]==s2[j-1] 且 dp[i][j-1] 为True，则dp[i][j]也为True。
+        由上面的状态转移方程可知，dp[i][j]只与dp[i-1][j]、dp[i][j-1]有关，因此，可用滚动数组来优化空间复杂度"""
+        if len(s1) < len(s2):
+            return self.isInterleave(s2, s1, s3)
         n1, n2, n3 = len(s1), len(s2), len(s3)
         if n1 + n2 != n3:
             return False
+        dp = [False] * (n2 + 1)
+        dp[0] = True
+        for i in range(n1 + 1):
+            for j in range(n2 + 1):
+                k = i + j - 1
+                if i > 0:
+                    # and 前面的dp[j]表示dp[i-1][j]，而 = 前面的dp[j]表示当前要求的dp[i][j]。这里将二维数组压缩成了一维数组
+                    dp[j] = dp[j] and s3[k] == s1[i - 1]
+                if j > 0 and not dp[j]:
+                    # and 前面的dp[j-1]表示dp[i][j-1]，而 = 前面的dp[j]表示当前要求的dp[i][j]。
+                    # 若dp[i][j]在上一步已经变为了True，则没必要再赋值了。
+                    dp[j] = dp[j - 1] and s3[k] == s2[j - 1]
+        return dp[-1]
+
+
+if __name__ == '__main__':
+    print(Solution().isInterleave(s1="aabcc", s2="dbbca", s3="aadbbcbcac"))
