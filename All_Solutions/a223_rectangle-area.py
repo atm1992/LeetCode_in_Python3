eeaefef4b835e15d0b6bf452a8fea19c2d86d693
Liu Quan 刘泉 <liuquan@wisers.com>
2022-04-11 23:25:34 +0800

    update

--- All_Solutions/a223_rectangle-area.py
+++ All_Solutions/a223_rectangle-area.py
@@ -22,4 +22,17 @@ Constraints:
 
 class Solution:
     def computeArea(self, ax1: int, ay1: int, ax2: int, ay2: int, bx1: int, by1: int, bx2: int, by2: int) -> int:
-        pass
+        """计算重叠面积。两个矩形覆盖的总面积等于两个矩形的面积之和减去两个矩形重叠部分的面积。"""
+        res = (ax2 - ax1) * (ay2 - ay1) + (bx2 - bx1) * (by2 - by1)
+        cx1 = max(ax1, bx1)
+        cy1 = max(ay1, by1)
+        cx2 = min(ax2, bx2)
+        cy2 = min(ay2, by2)
+        c_area = 0
+        if cx1 < cx2 and cy1 < cy2:
+            c_area = (cx2 - cx1) * (cy2 - cy1)
+        return res - c_area
+
+
+if __name__ == '__main__':
+    print(Solution().computeArea(ax1=-3, ay1=0, ax2=3, ay2=4, bx1=0, by1=-1, bx2=9, by2=2))
