e9f272260e1d2154c3d24aebcbfdc28b3cec5c9a
Liu Quan 刘泉 <liuquan@wisers.com>
2022-04-19 18:09:52 +0800

    update

--- All_Solutions/a282_expression-add-operators.py
+++ All_Solutions/a282_expression-add-operators.py
@@ -32,4 +32,48 @@ from typing import List
 class Solution:
     def addOperators(self, num: str, target: int) -> List[str]:
         """回溯"""
-        pass
+        n = len(num)
+        res = []
+
+        def backtrack(expression: List[str], idx: int, total: int, last_num: int) -> None:
+            """
+            expression 是由一个个字符组成的结果数组，idx 表示当前指向num的哪个下标。total 表示当前的计算结果，用于和target进行比较。
+            last_num 表示当前最后一个算入total的数，记录它的原因是 如果下一个符号是 * ，则需先把它从total减去，然后乘以下一个数，再算入total。
+            """
+            if idx == n:
+                if total == target:
+                    res.append(''.join(expression))
+                return
+            # 记录当前符号的idx
+            sign_idx = len(expression)
+            if idx > 0:
+                # 给待添加的符号占位，该符号的下标就是sign_idx，第一个数字前面不能有符号
+                expression.append('')
+            val = 0
+            for j in range(idx, n):
+                # 若idx位为0，则val只能等于0，后面不能再拼接其它字符
+                if j > idx and num[idx] == '0':
+                    break
+                ch = num[j]
+                val = val * 10 + int(ch)
+                expression.append(ch)
+                if idx == 0:
+                    # 若当前val为第一个数字，则total直接为val
+                    backtrack(expression, j + 1, val, val)
+                else:
+                    expression[sign_idx] = '+'
+                    backtrack(expression, j + 1, total + val, val)
+                    expression[sign_idx] = '-'
+                    # total - val 可看作是 total + (-val)，last_num 为 -val
+                    backtrack(expression, j + 1, total - val, -val)
+                    expression[sign_idx] = '*'
+                    backtrack(expression, j + 1, total - last_num + last_num * val, last_num * val)
+            # 回溯时，清除掉sign_idx及其之后的所有位
+            del expression[sign_idx:]
+
+        backtrack([], 0, 0, 0)
+        return res
+
+
+if __name__ == '__main__':
+    print(Solution().addOperators(num="232", target=8))
