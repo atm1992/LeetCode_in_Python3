c243da844275779601fac207db55e4042f3777b6
Liu Quan 刘泉 <liuquan@wisers.com>
2022-04-19 19:14:10 +0800

    update

--- All_Solutions/a283_move-zeroes.py
+++ All_Solutions/a283_move-zeroes.py
@@ -27,5 +27,18 @@ class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
         """
         Do not return anything, modify nums in-place instead.
+        双指针
         """
-        pass
+        n = len(nums)
+        left = right = 0
+        while right < n:
+            if nums[right] != 0:
+                nums[left], nums[right] = nums[right], nums[left]
+                left += 1
+            right += 1
+
+
+if __name__ == '__main__':
+    nums = [0, 1, 0, 3, 12]
+    Solution().moveZeroes(nums)
+    print(nums)
