6b3721e0c3f91638f05e8a9dc79902705c40f7d2
Liu Quan 刘泉 <liuquan@wisers.com>
2022-05-11 00:39:53 +0800

    update

---
+++ jian_zhi_offer_1/a42_lian-xu-zi-shu-zu-de-zui-da-he.py
@@ -0,0 +1,35 @@
+# -*- coding: UTF-8 -*-
+"""
+title: 连续子数组的最大和
+输入一个整型数组，数组中的一个或连续多个整数组成一个子数组。求所有子数组的和的最大值。
+要求时间复杂度为O(n)。
+
+
+示例1:
+输入: nums = [-2,1,-3,4,-1,2,1,-5,4]
+输出: 6
+解释: 连续子数组 [4,-1,2,1] 的和最大，为 6。
+
+
+提示：
+1 <= arr.length <= 10^5
+-100 <= arr[i] <= 100
+"""
+from typing import List
+
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        """贪心"""
+        res = float('-inf')
+        tmp = 0
+        for num in nums:
+            if tmp < 0:
+                tmp = 0
+            tmp += num
+            res = max(res, tmp)
+        return res
+
+
+if __name__ == '__main__':
+    print(Solution().maxSubArray([1, 2]))
