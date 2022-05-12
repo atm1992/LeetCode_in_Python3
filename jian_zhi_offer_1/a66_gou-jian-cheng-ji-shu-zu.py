529b255873068ba981ea818c03977bf102f64613
Liu Quan 刘泉 <liuquan@wisers.com>
2022-05-13 07:47:27 +0800

    update

--- jian_zhi_offer_1/a66_gou-jian-cheng-ji-shu-zu.py
+++ jian_zhi_offer_1/a66_gou-jian-cheng-ji-shu-zu.py
@@ -18,4 +18,41 @@ from typing import List
 
 class Solution:
     def constructArr(self, a: List[int]) -> List[int]:
-        pass
+        """使用两个数组，分别存储左边所有数字的乘积 以及 右边所有数字的乘积。"""
+        n = len(a)
+        left_product, right_product = [1] * n, [1] * n
+        for i in range(1, n):
+            left_product[i] = a[i - 1] * left_product[i - 1]
+            right_product[n - i - 1] = a[n - i] * right_product[n - i]
+        res = []
+        for i in range(n):
+            res.append(left_product[i] * right_product[i])
+        return res
+
+    def constructArr_2(self, a: List[int]) -> List[int]:
+        """为将空间复杂度降至O(1)，还可直接使用res来存储左边所有数字的乘积，然后使用一个变量right_product来跟踪右边元素的乘积"""
+        n = len(a)
+        res = [1] * n
+        for i in range(1, n):
+            res[i] = a[i - 1] * res[i - 1]
+        right_product = 1
+        for i in range(n - 2, -1, -1):
+            right_product *= a[i + 1]
+            res[i] *= right_product
+        return res
+
+    def constructArr_3(self, a: List[int]) -> List[int]:
+        """还可再将时间复杂度从O(2n)降至O(n)"""
+        n = len(a)
+        res = [1] * n
+        left_product = right_product = 1
+        for i in range(1, n):
+            left_product *= a[i - 1]
+            right_product *= a[n - i]
+            res[i] *= left_product
+            res[n - i - 1] *= right_product
+        return res
+
+
+if __name__ == '__main__':
+    print(Solution().constructArr_3([1, 2, 3, 4, 5]))
