205a13c21e6090152ef85ff5e2e36a551e9ad7ba
Liu Quan 刘泉 <liuquan@wisers.com>
2022-06-16 19:15:25 +0800

    update

--- jian_zhi_offer_2/a86_partition.py
+++ jian_zhi_offer_2/a86_partition.py
@@ -1,22 +1,26 @@
 # -*- coding: UTF-8 -*-
 """
-title: 分割回文串
-Given a string s, partition s such that every substring of the partition is a palindrome. Return all possible palindrome partitioning of s.
-A palindrome string is a string that reads the same backward as forward.
+title: 分割回文子字符串
+给定一个字符串 s ，请将 s 分割成一些子串，使每个子串都是 回文串 ，返回 s 所有可能的分割方案。
+回文串 是正着读和反着读都一样的字符串。
 
 
-Example 1:
-Input: s = "aab"
-Output: [["a","a","b"],["aa","b"]]
+示例 1：
+输入：s = "google"
+输出：[["g","o","o","g","l","e"],["g","oo","g","l","e"],["goog","l","e"]]
 
-Example 2:
-Input: s = "a"
-Output: [["a"]]
+示例 2：
+输入：s = "aab"
+输出：[["a","a","b"],["aa","b"]]
+
+示例 3：
+输入：s = "a"
+输出：[["a"]]
 
 
-Constraints:
+提示：
 1 <= s.length <= 16
-s contains only lowercase English letters.
+s 仅由小写英文字母组成
 """
 from functools import lru_cache
 from typing import List
