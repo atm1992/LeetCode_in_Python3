c0a9817e87dc3be7ef5df9238a53f4988021f295
Liu Quan 刘泉 <liuquan@wisers.com>
2022-06-16 21:06:24 +0800

    update

--- jian_zhi_offer_2/a90_rob.py
+++ jian_zhi_offer_2/a90_rob.py
@@ -1,27 +1,27 @@
 # -*- coding: UTF-8 -*-
 """
-title: 打家劫舍 II
-You are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed. All houses at this place are arranged in a circle. That means the first house is the neighbor of the last one. Meanwhile, adjacent houses have a security system connected, and it will automatically contact the police if two adjacent houses were broken into on the same night.
-Given an integer array nums representing the amount of money of each house, return the maximum amount of money you can rob tonight without alerting the police.
+title: 环形房屋偷盗
+一个专业的小偷，计划偷窃一个环形街道上沿街的房屋，每间房内都藏有一定的现金。这个地方所有的房屋都 围成一圈 ，这意味着第一个房屋和最后一个房屋是紧挨着的。同时，相邻的房屋装有相互连通的防盗系统，如果两间相邻的房屋在同一晚上被小偷闯入，系统会自动报警。
+给定一个代表每个房屋存放金额的非负整数数组 nums ，请计算 在不触动警报装置的情况下 ，今晚能够偷窃到的最高金额。
 
 
-Example 1:
-Input: nums = [2,3,2]
-Output: 3
-Explanation: You cannot rob house 1 (money = 2) and then rob house 3 (money = 2), because they are adjacent houses.
+示例 1：
+输入：nums = [2,3,2]
+输出：3
+解释：你不能先偷窃 1 号房屋（金额 = 2），然后偷窃 3 号房屋（金额 = 2）, 因为他们是相邻的。
 
-Example 2:
-Input: nums = [1,2,3,1]
-Output: 4
-Explanation: Rob house 1 (money = 1) and then rob house 3 (money = 3).
-Total amount you can rob = 1 + 3 = 4.
+示例 2：
+输入：nums = [1,2,3,1]
+输出：4
+解释：你可以先偷窃 1 号房屋（金额 = 1），然后偷窃 3 号房屋（金额 = 3）。
+     偷窃到的最高金额 = 1 + 3 = 4 。
 
-Example 3:
-Input: nums = [1,2,3]
-Output: 3
+示例 3：
+输入：nums = [0]
+输出：0
 
 
-Constraints:
+提示：
 1 <= nums.length <= 100
 0 <= nums[i] <= 1000
 """
@@ -34,7 +34,7 @@ class Solution:
         动态规划。
         dp[i] 表示偷窃前i间房屋所能获得的最高金额，状态转移方程：dp[i] = max(dp[i-2] + nums[i], dp[i-1])
         边界条件：dp[0] = nums[0], dp[1] = max(nums[0], nums[1])
-        本题与题198的区别在于：第一间房屋和最后一间房屋不能在同一晚上偷窃。因此可分为两种情况，分别进行动态规划，然后取两者的较大值。
+        本题与上一题的区别在于：第一间房屋和最后一间房屋不能在同一晚上偷窃。因此可分为两种情况，分别进行动态规划，然后取两者的较大值。
         1、计算 0 ~ n-2 的最高金额
         2、计算 1 ~ n-1 的最高金额
         """
@@ -56,7 +56,7 @@ class Solution:
         动态规划。
         dp[i] 表示偷窃前i间房屋所能获得的最高金额，状态转移方程：dp[i] = max(dp[i-2] + nums[i], dp[i-1])
         边界条件：dp[0] = nums[0], dp[1] = max(nums[0], nums[1])
-        本题与题198的区别在于：第一间房屋和最后一间房屋不能在同一晚上偷窃。因此可分为两种情况，分别进行动态规划，然后取两者的较大值。
+        本题与上一题的区别在于：第一间房屋和最后一间房屋不能在同一晚上偷窃。因此可分为两种情况，分别进行动态规划，然后取两者的较大值。
         1、计算 0 ~ n-2 的最高金额
         2、计算 1 ~ n-1 的最高金额
         """
@@ -76,4 +76,4 @@ class Solution:
 
 
 if __name__ == '__main__':
-    print(Solution().rob(nums=[1, 2, 3, 1]))
+    print(Solution().rob([1, 2, 3, 1]))
