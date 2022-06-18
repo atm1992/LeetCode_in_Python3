45ef75c71e44a3ef33eaf18264b28c30518253ff
Liu Quan 刘泉 <liuquan@wisers.com>
2022-06-18 20:51:23 +0800

    update

--- jian_zhi_offer_2/a96_isInterleave.py
+++ jian_zhi_offer_2/a96_isInterleave.py
@@ -1,35 +1,32 @@
 # -*- coding: UTF-8 -*-
 """
-title：交错字符串
-Given strings s1, s2, and s3, find whether s3 is formed by an interleaving of s1 and s2.
-
-An interleaving of two strings s and t is a configuration where they are divided into non-empty substrings such that:
+title: 字符串交织
+给定三个字符串 s1、s2、s3，请判断 s3 能不能由 s1 和 s2 交织（交错） 组成。
+两个字符串 s 和 t 交织 的定义与过程如下，其中每个字符串都会被分割成若干 非空 子字符串：
     s = s1 + s2 + ... + sn
     t = t1 + t2 + ... + tm
     |n - m| <= 1
-    The interleaving is s1 + t1 + s2 + t2 + s3 + t3 + ... or t1 + s1 + t2 + s2 + t3 + s3 + ...
-Note: a + b is the concatenation of strings a and b.
+    交织 是 s1 + t1 + s2 + t2 + s3 + t3 + ... 或者 t1 + s1 + t2 + s2 + t3 + s3 + ...
+提示：a + b 意味着字符串 a 和 b 连接。
 
 
-Example 1:
-Input: s1 = "aabcc", s2 = "dbbca", s3 = "aadbbcbcac"
-Output: true
+示例 1：
+输入：s1 = "aabcc", s2 = "dbbca", s3 = "aadbbcbcac"
+输出：true
 
-Example 2:
-Input: s1 = "aabcc", s2 = "dbbca", s3 = "aadbbbaccc"
-Output: false
+示例 2：
+输入：s1 = "aabcc", s2 = "dbbca", s3 = "aadbbbaccc"
+输出：false
 
-Example 3:
-Input: s1 = "", s2 = "", s3 = ""
-Output: true
+示例 3：
+输入：s1 = "", s2 = "", s3 = ""
+输出：true
 
 
-Constraints:
+提示：
 0 <= s1.length, s2.length <= 100
 0 <= s3.length <= 200
-s1, s2, and s3 consist of lowercase English letters.
-
-Follow up: Could you solve it using only O(s2.length) additional memory space?
+s1、s2、和 s3 都由小写英文字母组成
 """
 
 
