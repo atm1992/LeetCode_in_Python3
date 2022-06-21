2475f877b29417ba46c97119662018f3b34cc92e
Liu Quan 刘泉 <liuquan@wisers.com>
2022-06-21 18:50:38 +0800

    update

--- jian_zhi_offer_2/a98_uniquePaths.py
+++ jian_zhi_offer_2/a98_uniquePaths.py
@@ -1,36 +1,36 @@
 # -*- coding: UTF-8 -*-
 """
-title: 不同路径
-A robot is located at the top-left corner of a m x n grid (marked 'Start' in the diagram below).
-The robot can only move either down or right at any point in time. The robot is trying to reach the bottom-right corner of the grid (marked 'Finish' in the diagram below).
-How many possible unique paths are there?
+title: 路径的数目
+一个机器人位于一个 m x n 网格的左上角 （起始点在下图中标记为 “Start” ）。
+机器人每次只能向下或者向右移动一步。机器人试图达到网格的右下角（在下图中标记为 “Finish” ）。
+问总共有多少条不同的路径？
 
 
-Example 1:
-Input: m = 3, n = 7
-Output: 28
+示例 1：
+输入：m = 3, n = 7
+输出：28
 
-Example 2:
-Input: m = 3, n = 2
-Output: 3
-Explanation:
-From the top-left corner, there are a total of 3 ways to reach the bottom-right corner:
-1. Right -> Down -> Down
-2. Down -> Down -> Right
-3. Down -> Right -> Down
+示例 2：
+输入：m = 3, n = 2
+输出：3
+解释：
+从左上角开始，总共有 3 条路径可以到达右下角。
+1. 向右 -> 向下 -> 向下
+2. 向下 -> 向下 -> 向右
+3. 向下 -> 向右 -> 向下
 
-Example 3:
-Input: m = 7, n = 3
-Output: 28
+示例 3：
+输入：m = 7, n = 3
+输出：28
 
-Example 4:
-Input: m = 3, n = 3
-Output: 6
+示例 4：
+输入：m = 3, n = 3
+输出：6
 
 
-Constraints:
+提示：
 1 <= m, n <= 100
-It's guaranteed that the answer will be less than or equal to 2 * 10^9.
+题目数据保证答案小于等于 2 * 10^9
 """
 
 
@@ -64,4 +64,4 @@ class Solution:
 
 
 if __name__ == '__main__':
-    print(Solution().uniquePaths_2(m=3, n=7))
+    print(Solution().uniquePaths(m=3, n=3))
