2d963bc1eb8e62f85f372ab65b33b4364dd16b98
Liu Quan 刘泉 <liuquan@wisers.com>
2022-06-24 22:54:13 +0800

    update

--- jian_zhi_offer_2/a113_findOrder.py
+++ jian_zhi_offer_2/a113_findOrder.py
@@ -1,34 +1,36 @@
 # -*- coding: UTF-8 -*-
 """
-title: 课程表 II
-There are a total of numCourses courses you have to take, labeled from 0 to numCourses - 1. You are given an array prerequisites where prerequisites[i] = [ai, bi] indicates that you must take course bi first if you want to take course ai.
-    For example, the pair [0, 1], indicates that to take course 0 you have to first take course 1.
-Return the ordering of courses you should take to finish all courses. If there are many valid answers, return any of them. If it is impossible to finish all courses, return an empty array.
+title: 课程顺序
+现在总共有 numCourses 门课需要选，记为 0 到 numCourses-1。
+给定一个数组 prerequisites ，它的每一个元素 prerequisites[i] 表示两门课程之间的先修顺序。 例如 prerequisites[i] = [ai, bi] 表示想要学习课程 ai ，需要先完成课程 bi 。
+请根据给出的总课程数  numCourses 和表示先修顺序的 prerequisites 得出一个可行的修课序列。
+可能会有多个正确的顺序，只要任意返回一种就可以了。如果不可能完成所有课程，返回一个空数组。
 
 
-Example 1:
-Input: numCourses = 2, prerequisites = [[1,0]]
-Output: [0,1]
-Explanation: There are a total of 2 courses to take. To take course 1 you should have finished course 0. So the correct course order is [0,1].
+示例 1:
+输入: numCourses = 2, prerequisites = [[1,0]]
+输出: [0,1]
+解释: 总共有 2 门课程。要学习课程 1，你需要先完成课程 0。因此，正确的课程顺序为 [0,1] 。
 
-Example 2:
-Input: numCourses = 4, prerequisites = [[1,0],[2,0],[3,1],[3,2]]
-Output: [0,2,1,3]
-Explanation: There are a total of 4 courses to take. To take course 3 you should have finished both courses 1 and 2. Both courses 1 and 2 should be taken after you finished course 0.
-So one correct course order is [0,1,2,3]. Another correct ordering is [0,2,1,3].
+示例 2:
+输入: numCourses = 4, prerequisites = [[1,0],[2,0],[3,1],[3,2]]
+输出: [0,1,2,3] or [0,2,1,3]
+解释: 总共有 4 门课程。要学习课程 3，你应该先完成课程 1 和课程 2。并且课程 1 和课程 2 都应该排在课程 0 之后。
+ 因此，一个正确的课程顺序是 [0,1,2,3] 。另一个正确的排序是 [0,2,1,3] 。
 
-Example 3:
-Input: numCourses = 1, prerequisites = []
-Output: [0]
+示例 3:
+输入: numCourses = 1, prerequisites = []
+输出: [0]
+解释: 总共 1 门课，直接修第一门课就可。
 
 
-Constraints:
+提示:
 1 <= numCourses <= 2000
 0 <= prerequisites.length <= numCourses * (numCourses - 1)
 prerequisites[i].length == 2
 0 <= ai, bi < numCourses
 ai != bi
-All the pairs [ai, bi] are distinct.
+prerequisites 中不存在重复元素
 """
 from collections import defaultdict, deque
 from typing import List
@@ -93,7 +95,3 @@ class Solution:
                     break
         # return stack[::-1] if len(stack) == numCourses else []
         return [] if has_loop else stack[::-1]
-
-
-if __name__ == '__main__':
-    print(Solution().findOrder_2(numCourses=4, prerequisites=[[1, 0], [2, 0], [3, 1], [3, 2]]))
