98f838f9fbbc0e11b93c420e291cc7fbb9d0fc9f
Liu Quan 刘泉 <liuquan@wisers.com>
2022-06-29 14:29:43 +0800

    update

--- All_Solutions/a1997_first-day-where-you-have-been-in-all-the-rooms.py
+++ All_Solutions/a1997_first-day-where-you-have-been-in-all-the-rooms.py
@@ -1,6 +1,6 @@
 # -*- coding: UTF-8 -*-
 """
-title: 访问完所有房间的第一天。
+title: 访问完所有房间的第一天
 There are n rooms you need to visit, labeled from 0 to n - 1. Each day is labeled, starting from 0. You will go in and visit one room a day.
 Initially on day 0, you visit room 0. The order you visit the rooms for the coming days is determined by the following rules and a given 0-indexed array nextVisit of length n:
 Assuming that on a day, you visit room i,
