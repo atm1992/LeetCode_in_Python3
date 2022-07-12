7b58fc188e0ecbf50305bfd068fea067516776d2
Liu Quan 刘泉 <liuquan@wisers.com>
2022-07-12 21:15:33 +0800

    update

--- All_Solutions/a524_longest-word-in-dictionary-through-deleting.py
+++ All_Solutions/a524_longest-word-in-dictionary-through-deleting.py
@@ -24,4 +24,47 @@ from typing import List
 
 class Solution:
     def findLongestWord(self, s: str, dictionary: List[str]) -> str:
-        pass
+        """双指针"""
+        # 先按长度降序，再按字典序升序
+        dictionary.sort(key=lambda x: (-len(x), x))
+        n = len(s)
+        for word in dictionary:
+            m = len(word)
+            i = j = 0
+            while i < n and j < m:
+                if s[i] == word[j]:
+                    j += 1
+                i += 1
+            if j == m:
+                return word
+        return ''
+
+    def findLongestWord_2(self, s: str, dictionary: List[str]) -> str:
+        """
+        动态规划。参考LeetCode题392
+        针对字符串s预处理出一个二维dp数组，之后对于dictionary中的每个字符串t，均可借助二维dp数组，线性计算出t是否为s的子序列。
+        dp[i][j] 表示从字符串s的位置i(含)开始，第一次出现字符j的位置。j=0表示字符'a', j=25表示字符'z'，0<=i<=n-1, n = len(s)
+        状态转移方程：若 s[i] 恰好就是字符j，则 dp[i][j] = i；否则 dp[i][j] = dp[i+1][j]。如果之后都不存在字符j，则 dp[i][j] = n
+        显然，i 需要从后往前遍历
+        初始值：dp[n][*] = n
+        """
+        # 先按长度降序，再按字典序升序
+        dictionary.sort(key=lambda x: (-len(x), x))
+        n = len(s)
+        dp = [[0] * 26 for _ in range(n)] + [[n] * 26]
+        for i in range(n - 1, -1, -1):
+            for j in range(26):
+                dp[i][j] = i if ord(s[i]) - ord('a') == j else dp[i + 1][j]
+        for word in dictionary:
+            idx = 0
+            for ch in word:
+                if dp[idx][ord(ch) - ord('a')] == n:
+                    break
+                idx = dp[idx][ord(ch) - ord('a')] + 1
+            else:
+                return word
+        return ''
+
+
+if __name__ == '__main__':
+    print(Solution().findLongestWord(s="abpcplea", dictionary=["ale", "apple", "monkey", "plea"]))
