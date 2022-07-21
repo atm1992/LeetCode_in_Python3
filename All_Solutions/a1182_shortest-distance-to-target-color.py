54a42cc0b9f65c42425f25027ad3f5dbda38be19
Liu Quan 刘泉 <liuquan@wisers.com>
2022-07-21 12:01:18 +0800

    update

--- All_Solutions/a1182_shortest-distance-to-target-color.py
+++ All_Solutions/a1182_shortest-distance-to-target-color.py
@@ -32,4 +32,28 @@ from typing import List
 
 class Solution:
     def shortestDistanceColor(self, colors: List[int], queries: List[List[int]]) -> List[int]:
-        pass
+        """左右各扫描一次，预处理出colors中所有元素与3种颜色的最短距离。"""
+        n = len(colors)
+        # 总共有3种颜色
+        m = 3
+        dist = [[-1] * m for _ in range(n)]
+        left = [-1] * m
+        for i in range(n):
+            left[colors[i] - 1] = i
+            for j in range(m):
+                if left[j] != -1:
+                    dist[i][j] = i - left[j]
+        right = [n] * m
+        for i in range(n - 1, -1, -1):
+            right[colors[i] - 1] = i
+            for j in range(m):
+                if right[j] != n and (dist[i][j] == -1 or right[j] - i < dist[i][j]):
+                    dist[i][j] = right[j] - i
+        res = []
+        for i, c in queries:
+            res.append(dist[i][c - 1])
+        return res
+
+
+if __name__ == '__main__':
+    print(Solution().shortestDistanceColor(colors=[1, 2], queries=[[0, 3]]))
