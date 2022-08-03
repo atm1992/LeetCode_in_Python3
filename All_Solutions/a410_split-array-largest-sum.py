ec348e3d0c5198fb20cdc0528041b0210d6858ff
Liu Quan 刘泉 <liuquan@wisers.com>
2022-08-03 17:10:43 +0800

    update

--- All_Solutions/a410_split-array-largest-sum.py
+++ All_Solutions/a410_split-array-largest-sum.py
@@ -32,4 +32,37 @@ from typing import List
 
 class Solution:
     def splitArray(self, nums: List[int], m: int) -> int:
-        pass
+        """二分猜答案(二分查找 + 贪心)"""
+
+        def check(sub_sum: int) -> bool:
+            """验证是否存在分割子数组的最大和不超过sub_sum的分隔方案。若最终的子数组个数小于等于m，则说明方案可行，可以继续减小sub_sum"""
+            cnt = tmp_sum = 0
+            for num in nums:
+                # 保证每个tmp_sum都小于等于sub_sum
+                if tmp_sum + num > sub_sum:
+                    cnt += 1
+                    # 若当前已切分出m个子数组，则最终的子数组个数一定大于m，因为当前的 tmp_sum = num > 0
+                    if cnt == m:
+                        return False
+                    # 只有当num大于0时，才可能会切分出一个新的子数组。若num为0，则会直接将其划分到上一个子数组
+                    tmp_sum = num
+                else:
+                    tmp_sum += num
+            return True
+
+        total = sum(nums)
+        if m == 1:
+            return total
+        left, right = max(max(nums), total // m), total
+        while left < right:
+            mid = (left + right) // 2
+            if check(mid):
+                # 说明方案可行，可以继续减小
+                right = mid
+            else:
+                left = mid + 1
+        return left
+
+
+if __name__ == '__main__':
+    print(Solution().splitArray(nums=[7, 2, 5, 10, 8], m=2))
