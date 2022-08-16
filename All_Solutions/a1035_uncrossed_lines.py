f270967863a8a51ed9b64bd42eb17ceb944c912b
Liu Quan 刘泉 <liuquan@wisers.com>
2022-08-16 17:56:17 +0800

    update

--- All_Solutions/a1035_uncrossed_lines.py
+++ All_Solutions/a1035_uncrossed_lines.py
@@ -33,4 +33,26 @@ from typing import List
 
 class Solution:
     def maxUncrossedLines(self, nums1: List[int], nums2: List[int]) -> int:
-        pass
+        """
+        二维动态规划
+        k条互不相交的连线会连接nums1和nums2的k对相等的元素，即 nums1中的k个元素与nums2中的k个元素，它们的相对顺序是一致的。
+        因此，这k对相等的元素其实就是nums1和nums2的最长公共子序列，可使用二维动态规划来求解最长公共子序列问题，参考LeetCode题1143
+        dp[i][j] 表示nums1中前i个元素(下标i-1)与nums2中前j个元素(下标j-1)之间最长公共子序列的长度。
+        边界情况：空数组与任意数组之间最长公共子序列的长度均为0，所以 dp[0][*] = dp[*][0] = 0
+        状态转移方程：若nums1[i-1] == nums2[j-1]，则 dp[i][j] = dp[i-1][j-1] + 1；
+        若nums[i-1] != nums[j-1]，则 dp[i][j] = max(dp[i-1][j], dp[i][j-1])。
+        相当于 用nums1中前i-1个元素与nums2中前j个元素去匹配 以及 用nums1中前i个元素与nums2中前j-1个元素去匹配
+        """
+        m, n = len(nums1), len(nums2)
+        dp = [[0] * (n + 1) for _ in range(m + 1)]
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                if nums1[i - 1] == nums2[j - 1]:
+                    dp[i][j] = dp[i - 1][j - 1] + 1
+                else:
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+        return dp[-1][-1]
+
+
+if __name__ == '__main__':
+    print(Solution().maxUncrossedLines(nums1=[2, 5, 1, 2, 5], nums2=[10, 5, 2, 1, 5, 2]))
--- All_Solutions/a4_median_of_two_sorted_arrays.py
+++ All_Solutions/a4_median_of_two_sorted_arrays.py
@@ -40,66 +40,65 @@ from typing import List
 
 
 class Solution:
-    # 这种解法虽然空间复杂度为O(1)，但时间复杂度为O(m+n)，因此不符合要求。
-    # 时间复杂度要求为O(log (m+n))，看到有log，并且是有序数组，第一想法就应该是二分查找。
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        len1, len2 = len(nums1), len(nums2)
-        p1, p2 = 0, 0
-        n1, n2 = 0, 0
-        while p1 + p2 <= int((len1 + len2) / 2):
-            n1 = n2
-            if p1 < len1 and p2 < len2:
+        """顺序扫描。虽然空间复杂度为O(1)，但时间复杂度为O(m+n)，因此不符合题目要求。"""
+        m, n = len(nums1), len(nums2)
+        p1 = p2 = 0
+        pre = cur = 0
+        for _ in range((m + n) // 2 + 1):
+            pre = cur
+            if p1 < m and p2 < n:
                 if nums1[p1] <= nums2[p2]:
-                    n2 = nums1[p1]
+                    cur = nums1[p1]
                     p1 += 1
                 else:
-                    n2 = nums2[p2]
+                    cur = nums2[p2]
                     p2 += 1
-            elif p1 < len1:
-                n2 = nums1[p1]
+            elif p1 < m:
+                cur = nums1[p1]
                 p1 += 1
             else:
-                n2 = nums2[p2]
+                cur = nums2[p2]
                 p2 += 1
-        return n2 if (len1 + len2) % 2 == 1 else (n1 + n2) / 2
+        return cur if (m + n) & 1 else (pre + cur) / 2
 
     def findMedianSortedArrays_2(self, nums1: List[int], nums2: List[int]) -> float:
-        """找到nums1与nums2之间的较短数组，对其进行二分查找，确定了一个数组的下标，自然也就确定了另一个数组的下标，因为中位数的下标是确定的"""
-        m, n = len(nums1), len(nums2)
-        if m > n:
+        """
+        二分查找
+        题目要求时间复杂度为O(log (m+n))，看到有log，并且是有序数组，第一想法就应该是二分查找。
+        找到nums1与nums2之间的较短数组，对其进行二分查找，确定了一个数组的下标，自然也就确定了另一个数组的下标，因为中位数的下标是确定的。
+        用i与j分别去划分数组nums1与nums2，划分为左半边（[0, i) + [0, j)）和右半边（[i, m) + [j, n)）。
+        若m+n为偶数，则 i + j == m-i + n-j，即 i + j == (m+n)/2；此时的中位数为左半边的最大值与右半边的最小值相加后除以2
+        若m+n为奇数，则 i + j == m-i + n-j + 1 (多出的那个元素放到左半边)，即 i + j == (m+n+1)/2；此时的中位数就是左半边的最大值(也就是左半边多出的那个元素)
+        对于m+n为偶数来说，(m+n)/2 等价于 (m+n+1)//2；对于m+n为奇数来说，(m+n+1)/2 也等价于 (m+n+1)//2；所以，无论m+n是偶数还是奇数，都有 i + j == (m+n+1)//2。
+        i 的取值范围：[0, m]。i取0时，[0, i)为空数组，即 将整个nums1划分到右半边；i取m时，[i, m)为空数组，即 将整个nums1划分到左半边。初始时，i 可取0与m之间的中间位置。
+        时间复杂度为 O(log min(m, n))。
+        """
+        if len(nums1) > len(nums2):
             return self.findMedianSortedArrays_2(nums2, nums1)
+        m, n = len(nums1), len(nums2)
+        median_idx = (m + n + 1) // 2
         # -10^6 <= nums1[i], nums2[i] <= 10^6
         infinity = 10 ** 6 + 1
-        left, right = 0, m
-        # 分别表示 左边的最大值与右边的最小值
+        # 分别记录 左半边的最大值、右半边的最小值
         max_left, min_right = 0, 0
-        # 跳出循环时，left = right + 1
+        left, right = 0, m
         while left <= right:
-            """
-            用i与j分别去划分数组nums1与nums2，划分为 [0 ~ i-1] [i ~ m-1] 以及 [0 ~ j-1] [j ~ n-1]。
-            若m+n为偶数，则 i-1-0 + j-1-0 == m-1-i + n-1-j，即 i + j == (m+n)/2；此时的中位数为左边的最大值与右边的最小值相加后除以2
-            若m+n为奇数，则 i-1-0 + j-1-0 == m-1-i + n-1-j + 1 (多的那一个元素放到左边)，即 i + j == (m+n+1)/2；此时的中位数就是左边的最大值(也就是左边多出来的那一个元素)
-            对于m+n为偶数来说，(m+n)/2 等价于 (m+n+1)//2；对于m+n为奇数来说，(m+n+1)/2 也等价于 (m+n+1)//2；
-            因此，无论m+n是偶数还是奇数，都有 i + j == (m+n+1)//2。
-            i 可以取值 0 ~ m，取值0表示将整个数组划到右边；取值m表示将整个数组划到左边。所以刚开始时，i 取 0与m 之间的中间位置。
-            时间复杂度为 O(log min(m, n))。
-            """
-            # 这里 // 2 的目的是避免i为小数
             i = (left + right) // 2
-            j = (m + n + 1) // 2 - i
-            nums_i_1 = -infinity if i == 0 else nums1[i-1]
-            nums_i = infinity if i == m else nums1[i]
-            nums_j_1 = -infinity if j == 0 else nums2[j-1]
-            nums_j = infinity if j == n else nums2[j]
-            if nums_i_1 <= nums_j:
-                max_left, min_right = max(nums_i_1, nums_j_1), min(nums_i, nums_j)
+            j = median_idx - i
+            nums1_left = -infinity if i == 0 else nums1[i - 1]
+            nums1_right = infinity if i == m else nums1[i]
+            nums2_left = -infinity if j == 0 else nums2[j - 1]
+            nums2_right = infinity if j == n else nums2[j]
+            if nums1_left <= nums2_right:
+                max_left, min_right = max(nums1_left, nums2_left), min(nums1_right, nums2_right)
+                # i 向右移，逐渐增大nums1_left、nums1_right
                 left = i + 1
             else:
+                # i 向左移，逐渐减小nums1_left、nums1_right
                 right = i - 1
-        return (max_left + min_right) / 2 if (m+n) % 2 == 0 else max_left
+        return max_left if (m + n) & 1 else (max_left + min_right) / 2
 
 
 if __name__ == '__main__':
-    nums1 = [2]
-    nums2 = []
-    print(Solution().findMedianSortedArrays_2(nums1, nums2))
+    print(Solution().findMedianSortedArrays_2([2], []))
