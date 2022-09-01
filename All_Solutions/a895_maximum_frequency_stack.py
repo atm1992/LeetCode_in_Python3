cb3dad8ab4d68cf13f233909f582e84ff6389db3
Liu Quan 刘泉 <liuquan@wisers.com>
2022-09-01 11:24:16 +0800

    update

--- All_Solutions/a895_maximum_frequency_stack.py
+++ All_Solutions/a895_maximum_frequency_stack.py
@@ -34,20 +34,61 @@ Constraints:
 At most 2 * 10^4 calls will be made to push and pop.
 It is guaranteed that there will be at least one element in the stack before calling pop.
 """
+import heapq
+from collections import defaultdict
 
 
 class FreqStack:
+    """优先队列(最大堆) + 哈希表"""
 
     def __init__(self):
-        pass
+        self.idx = 0
+        self.nums = []
+        self.num2freq = defaultdict(int)
 
     def push(self, val: int) -> None:
-        pass
+        self.num2freq[val] += 1
+        self.idx += 1
+        heapq.heappush(self.nums, (-self.num2freq[val], -self.idx, val))
 
     def pop(self) -> int:
-        pass
+        _, _, num = heapq.heappop(self.nums)
+        self.num2freq[num] -= 1
+        return num
 
-# Your FreqStack object will be instantiated and called as such:
-# obj = FreqStack()
-# obj.push(val)
-# param_2 = obj.pop()
+
+class FreqStack2:
+    """栈 + 哈希表"""
+
+    def __init__(self):
+        self.num2freq = defaultdict(int)
+        self.freq2nums = defaultdict(list)
+        self.max_freq = 0
+
+    def push(self, val: int) -> None:
+        self.num2freq[val] += 1
+        if self.num2freq[val] > self.max_freq:
+            self.max_freq = self.num2freq[val]
+        self.freq2nums[self.num2freq[val]].append(val)
+
+    def pop(self) -> int:
+        num = self.freq2nums[self.max_freq].pop()
+        self.num2freq[num] -= 1
+        if not self.freq2nums[self.max_freq]:
+            # 因为max_freq是一步步涨上去的，所以减也是一步步减
+            self.max_freq -= 1
+        return num
+
+
+if __name__ == '__main__':
+    obj = FreqStack()
+    obj.push(5)
+    obj.push(7)
+    obj.push(5)
+    obj.push(7)
+    obj.push(4)
+    obj.push(5)
+    print(obj.pop())
+    print(obj.pop())
+    print(obj.pop())
+    print(obj.pop())
