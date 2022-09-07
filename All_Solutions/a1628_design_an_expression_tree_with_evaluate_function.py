86bde57707ed01e3e68c0477279ee278da4e5967
Liu Quan 刘泉 <liuquan@wisers.com>
2022-09-07 18:53:07 +0800

    update

--- All_Solutions/a1628_design_an_expression_tree_with_evaluate_function.py
+++ All_Solutions/a1628_design_an_expression_tree_with_evaluate_function.py
@@ -30,7 +30,7 @@ The absolute value of the result and intermediate values will not exceed 10^9.
 It is guaranteed that no expression will include division by zero.
 """
 from abc import ABC, abstractmethod
-from typing import List
+from typing import List, Optional
 
 """
 This is the interface for the expression tree Node.
@@ -45,21 +45,48 @@ class Node(ABC):
         pass
 
 
+class TreeNode(Node):
+    def __init__(self, val: str):
+        self.val = val
+        self.left = None
+        self.right = None
+
+    def evaluate(self) -> int:
+        if self.val.isdigit():
+            return int(self.val)
+        elif self.val == '+':
+            return self.left.evaluate() + self.right.evaluate()
+        elif self.val == '-':
+            return self.left.evaluate() - self.right.evaluate()
+        elif self.val == '*':
+            return self.left.evaluate() * self.right.evaluate()
+        else:
+            return self.left.evaluate() // self.right.evaluate()
+
+
 """    
 This is the TreeBuilder class.
 You can treat it as the driver code that takes the postinfix input
-and returns the expression tree represnting it as a Node.
+and returns the expression tree representing it as a Node.
 """
 
 
 class TreeBuilder(object):
-    def buildTree(self, postfix: List[str]) -> 'Node':
-        pass
+    def buildTree(self, postfix: List[str]) -> Optional[Node]:
+        """
+        因为给定的postfix是个后序序列（左 - 右 - 根），所以构建树时，按 根 - 右 - 左 的顺序不断pop postfix
+        """
+        if not postfix:
+            return None
+        node = TreeNode(postfix.pop())
+        # 叶节点表示操作数，非叶节点表示运算符
+        if node.val.isdigit():
+            return node
+        node.right = self.buildTree(postfix)
+        node.left = self.buildTree(postfix)
+        return node
 
 
-"""
-Your TreeBuilder object will be instantiated and called as such:
-obj = TreeBuilder();
-expTree = obj.buildTree(postfix);
-ans = expTree.evaluate();
-"""
+if __name__ == '__main__':
+    root = TreeBuilder().buildTree(["4", "5", "2", "7", "+", "-", "*"])
+    print(root.evaluate())
