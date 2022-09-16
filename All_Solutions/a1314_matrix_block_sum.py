9b53a03b1bb873b98e3a9744e55e398f40665d19
Liu Quan 刘泉 <liuquan@wisers.com>
2022-09-16 11:22:10 +0800

    update

--- All_Solutions/a1314_matrix_block_sum.py
+++ All_Solutions/a1314_matrix_block_sum.py
@@ -27,4 +27,21 @@ from typing import List
 
 class Solution:
     def matrixBlockSum(self, mat: List[List[int]], k: int) -> List[List[int]]:
-        pass
+        """二维前缀和。参考LeetCode题304"""
+        m, n = len(mat), len(mat[0])
+        pre_mat = [[0] * (n + 1) for _ in range(m + 1)]
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                pre_mat[i][j] = pre_mat[i - 1][j] + pre_mat[i][j - 1] + mat[i - 1][j - 1] - pre_mat[i - 1][j - 1]
+        res = [[0] * n for _ in range(m)]
+        for i in range(m):
+            for j in range(n):
+                min_r, max_r = max(i - k, 0), min(i + k, m - 1) + 1
+                min_c, max_c = max(j - k, 0), min(j + k, n - 1) + 1
+                # 右下角矩形的面积 = 整体面积 - 上面面积 - 左边面积 + 左上面积
+                res[i][j] = pre_mat[max_r][max_c] - pre_mat[min_r][max_c] - pre_mat[max_r][min_c] + pre_mat[min_r][min_c]
+        return res
+
+
+if __name__ == '__main__':
+    print(Solution().matrixBlockSum(mat=[[1, 2, 3], [4, 5, 6], [7, 8, 9]], k=2))
