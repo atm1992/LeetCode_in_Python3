72cc01aa8d03ec2a49e3cdbfcc3ba902874ba7d9
Liu Quan 刘泉 <liuquan@wisers.com>
2022-10-19 08:34:53 +0800

    update

--- All_Solutions/a366_find_leaves_of_binary_tree.py
+++ All_Solutions/a366_find_leaves_of_binary_tree.py
@@ -22,6 +22,7 @@ Constraints:
 The number of nodes in the tree is in the range [1, 100].
 -100 <= Node.val <= 100
 """
+from collections import deque
 from typing import List, Optional
 
 
@@ -35,4 +36,44 @@ class TreeNode:
 
 class Solution:
     def findLeaves(self, root: Optional[TreeNode]) -> List[List[int]]:
-        pass
+
+        def dfs(node: Optional[TreeNode]) -> int:
+            """递归计算所有节点的高度，叶节点的高度为0，高度越大，其子孙节点就越多，自然也就越晚删除"""
+            if not node:
+                return -1
+            depth = max(dfs(node.left), dfs(node.right)) + 1
+            # 可以肯定的是，depth是逐渐变化的，要么在上一个节点的基础加1，要么减1。
+            if depth == len(res):
+                res.append([])
+            res[depth].append(node.val)
+            return depth
+
+        res = []
+        dfs(root)
+        return res
+
+
+def bfs_build_binary_tree(bfs_li: List[int]) -> Optional[TreeNode]:
+    """使用层次序列构建二叉树。所输入的层次序列任意，可以有None，可以重复"""
+    if not bfs_li or bfs_li[0] is None:
+        return None
+    bfs_li = deque(bfs_li)
+    root = TreeNode(bfs_li.popleft())
+    nodes = deque([root])
+    while bfs_li:
+        cur_node = nodes.popleft()
+        val = bfs_li.popleft()
+        if val is not None:
+            nodes.append(TreeNode(val))
+            cur_node.left = nodes[-1]
+        if bfs_li:
+            val = bfs_li.popleft()
+            if val is not None:
+                nodes.append(TreeNode(val))
+                cur_node.right = nodes[-1]
+    return root
+
+
+if __name__ == '__main__':
+    root = bfs_build_binary_tree([1, 2, 3, 4, 5])
+    print(Solution().findLeaves(root))
