33b2dfa46604f5a081d35e1f9cc9a37f6c183947
Liu Quan 刘泉 <liuquan@wisers.com>
2022-10-22 22:37:26 +0800

    update

--- All_Solutions/a527_word_abbreviation.py
+++ All_Solutions/a527_word_abbreviation.py
@@ -1,6 +1,6 @@
 # -*- coding: UTF-8 -*-
 """
-title:
+title: 单词缩写
 Given an array of distinct strings words, return the minimal possible abbreviations for every word.
 The following are the rules for a string abbreviation:
     The initial abbreviation for each word is: the first character, then the number of characters in between, followed by the last character.
@@ -26,9 +26,53 @@ Constraints:
 words[i] consists of lowercase English letters.
 All the strings of words are unique.
 """
+from collections import defaultdict
 from typing import List
+
+
+class Trie:
+    def __init__(self):
+        self.children = defaultdict(Trie)
+        self.word_cnt = 0
+
+    def insert(self, word: str) -> None:
+        node = self
+        for ch in word:
+            node = node.children[ch]
+            node.word_cnt += 1
 
 
 class Solution:
     def wordsAbbreviation(self, words: List[str]) -> List[str]:
-        pass
+        """
+        分组 + 字典树
+        """
+        groups = defaultdict(list)
+        # 先对words按 (word长度, word首字母, word尾字母) 进行分组
+        for idx, word in enumerate(words):
+            groups[(len(word), word[0], word[-1])].append((word, idx))
+        res = [''] * len(words)
+        for group in groups.values():
+            # 针对每个分组，建一棵字典树
+            root = Trie()
+            # 先将当前分组中的所有word都插入到字典树中
+            for word, _ in group:
+                root.insert(word)
+            # 遍历当前分组中的所有word
+            for word, idx in group:
+                node = root
+                for i, ch in enumerate(word):
+                    # 若当前字母是当前word在同组的所有word中独有的，则说明可以得到当前word唯一的缩写了。
+                    # 因为原始的words中不存在两个完全相同的word，并且同组的所有word的长度是相同的，
+                    # 所以在同组中不存在一个word是另一个word的前缀问题，因此下面这个条件一定是成立的
+                    if node.children[ch].word_cnt == 1:
+                        abbr_len = len(word) - i - 2
+                        abbr = word[:i + 1] + str(abbr_len) + word[-1] if abbr_len > 1 else word
+                        res[idx] = abbr
+                        break
+                    node = node.children[ch]
+        return res
+
+
+if __name__ == '__main__':
+    print(Solution().wordsAbbreviation(words=["like", "god", "internal", "me", "internet", "interval", "intension", "face", "intrusion"]))
