33d318ba7037ab2b3b62b55d5ea5069671cad2be
Liu Quan 刘泉 <liuquan@wisers.com>
2022-11-01 12:21:04 +0800

    update

---
+++ All_Solutions/a625_minimum_factorization.py
@@ -0,0 +1,23 @@
+# -*- coding: UTF-8 -*-
+"""
+title: 最小因式分解
+Given a positive integer num, return the smallest positive integer x whose multiplication of each digit equals num. If there is no answer or the answer is not fit in 32-bit signed integer, return 0.
+
+
+Example 1:
+Input: num = 48
+Output: 68
+
+Example 2:
+Input: num = 15
+Output: 35
+
+
+Constraints:
+1 <= num <= 2^31 - 1
+"""
+
+
+class Solution:
+    def smallestFactorization(self, num: int) -> int:
+        pass
--- All_Solutions/a670_maximum_swap.py
+++ All_Solutions/a670_maximum_swap.py
@@ -23,4 +23,22 @@ Constraints:
 
 class Solution:
     def maximumSwap(self, num: int) -> int:
-        pass
+        """贪心。待交换的大数字必须在待交换的小数字右侧，并尽量大；待交换的小数字必须比待交换的大数字小，并尽量靠左。"""
+        nums = list(str(num))
+        n = len(nums)
+        max_idx = n - 1
+        l_idx, r_idx = -1, -1
+        for i in range(n - 2, -1, -1):
+            if nums[i] > nums[max_idx]:
+                max_idx = i
+            elif nums[i] < nums[max_idx]:
+                # l_idx 尽量靠左，nums[r_idx] 尽量大
+                l_idx, r_idx = i, max_idx
+        if l_idx == -1:
+            return num
+        nums[l_idx], nums[r_idx] = nums[r_idx], nums[l_idx]
+        return int(''.join(nums))
+
+
+if __name__ == '__main__':
+    print(Solution().maximumSwap(6436))
