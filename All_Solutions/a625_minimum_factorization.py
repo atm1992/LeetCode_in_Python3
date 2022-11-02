3bf59b2fcb2316ec89c1dc73edf484d0dfb06375
Liu Quan 刘泉 <liuquan@wisers.com>
2022-11-02 11:57:02 +0800

    update

--- All_Solutions/a625_minimum_factorization.py
+++ All_Solutions/a625_minimum_factorization.py
@@ -20,4 +20,27 @@ Constraints:
 
 class Solution:
     def smallestFactorization(self, num: int) -> int:
-        pass
+        """
+        贪心 + 因式分解。
+        从因数9到因数2逐步分解num，注意：并不是质因数分解。
+        根据测试结果发现，若num只有一位数，则直接返回num本身，而不是返回 10 + num
+        """
+        if num < 10:
+            return num
+        res, weight = 0, 1
+        for f in range(9, 1, -1):
+            while num % f == 0:
+                num //= f
+                # 因为是倒序从9分解到2，所以越往后的因数在res中应该越靠左，这样才能使res最小
+                res += weight * f
+                weight *= 10
+                if res > 2 ** 31 - 1:
+                    return 0
+            if num == 1:
+                return res
+        # num无法完全被9到2分解
+        return 0
+
+
+if __name__ == '__main__':
+    print(Solution().smallestFactorization(13))
