42b59cc60e31db7daea8b4972db93041b9f1f135
Liu Quan 刘泉 <liuquan@wisers.com>
2022-11-09 13:12:22 +0800

    update

--- All_Solutions/a719_find_k_th_smallest_pair_distance.py
+++ All_Solutions/a719_find_k_th_smallest_pair_distance.py
@@ -34,4 +34,31 @@ from typing import List
 
 class Solution:
     def smallestDistancePair(self, nums: List[int], k: int) -> int:
-        pass
+        """排序 + 二分查找 + 双指针"""
+
+        def check(dist: int) -> int:
+            """使用双指针统计距离小于等于dist的数对数量"""
+            cnt = i = 0
+            for j, num in enumerate(nums):
+                # 退出此while循环时，num - nums[i] <= dist
+                while num - nums[i] > dist:
+                    i += 1
+                # 下标范围[i, j]内的数对数量为 j - i，为避免重复统计，每次都是固定数对的右端点下标j，因为每次for循环，j都会加1，即 每次for循环中的j都是不同的，从而保证了不重复
+                # (nums[i], nums[j])、(nums[i+1], nums[j])、……、(nums[j-1], nums[j])。
+                cnt += j - i
+            return cnt
+
+        nums.sort()
+        left, right = 0, nums[-1] - nums[0]
+        # 第 K 小的数对距离一定在 [left, right] 范围内
+        while left < right:
+            mid = (left + right) >> 1
+            if check(mid) >= k:
+                right = mid
+            else:
+                left = mid + 1
+        return left
+
+
+if __name__ == '__main__':
+    print(Solution().smallestDistancePair(nums=[1, 6, 1], k=3))
