0e41d144382bed1b7ff664281954e6419b69f483
Liu Quan 刘泉 <liuquan@wisers.com>
2022-11-15 14:37:02 +0800

    update

--- All_Solutions/a769_max_chunks_to_make_sorted.py
+++ All_Solutions/a769_max_chunks_to_make_sorted.py
@@ -32,4 +32,19 @@ from typing import List
 
 class Solution:
     def maxChunksToSorted(self, arr: List[int]) -> int:
-        pass
+        """
+        贪心
+        由题意可知，arr中总共有n个元素，这些元素的范围为[0, n-1]，并且各个元素互不相同，所以对arr排序后，有 arr[i] = i
+        因此，若arr的某个前缀块arr[0:i]的最大值等于下标i，则说明对该前缀块排序后与原数组排序后的结果一致。
+        统计这些前缀块的数量，即可得到最终结果。
+        """
+        res = max_num = 0
+        for i, num in enumerate(arr):
+            max_num = max(max_num, num)
+            if max_num == i:
+                res += 1
+        return res
+
+
+if __name__ == '__main__':
+    print(Solution().maxChunksToSorted([1, 0, 2, 3, 4]))
