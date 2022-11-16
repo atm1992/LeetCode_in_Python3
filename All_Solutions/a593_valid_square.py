bd04c7a62e9f895512e54469f650d0643bc529bb
Liu Quan 刘泉 <liuquan@wisers.com>
2022-11-16 15:54:33 +0800

    update

--- All_Solutions/a593_valid_square.py
+++ All_Solutions/a593_valid_square.py
@@ -23,9 +23,27 @@ Constraints:
 p1.length == p2.length == p3.length == p4.length == 2
 -10^4 <= xi, yi <= 10^4
 """
+from collections import Counter
 from typing import List
 
 
 class Solution:
     def validSquare(self, p1: List[int], p2: List[int], p3: List[int], p4: List[int]) -> bool:
-        pass
+        """
+        数学
+        4个点可以连接出6条边。4条相等的边可能是正方形，也可能是菱形。再加上2条相等的对角线，就可排除菱形。并且对角线的长度为 根号2 * 边长
+        """
+
+        def get_dist(p1: List[int], p2: List[int]) -> int:
+            # 注意：这里计算长度时，没有开根号
+            return (p1[0] - p2[0]) ** 2 + (p1[1] - p2[1]) ** 2
+
+        dists = [get_dist(p1, p2), get_dist(p1, p3), get_dist(p1, p4), get_dist(p2, p3), get_dist(p2, p4), get_dist(p3, p4)]
+        dist2cnt = Counter(dists)
+        edge, diagonal = min(dists), max(dists)
+        # 因为计算长度时没有开根号，所以这里是乘以2，而不是乘以根号2
+        return dist2cnt[edge] == 4 and dist2cnt[diagonal] == 2 and diagonal == 2 * edge
+
+
+if __name__ == '__main__':
+    print(Solution().validSquare(p1=[1134, -2539], p2=[492, -1255], p3=[-792, -1897], p4=[-150, -3181]))
