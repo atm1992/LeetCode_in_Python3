cd525c55b1a53d09d78db2a6467ed71080bfd29e
Liu Quan 刘泉 <liuquan@wisers.com>
2022-11-19 10:20:49 +0800

    update

--- All_Solutions/a316_remove_duplicate_letters.py
+++ All_Solutions/a316_remove_duplicate_letters.py
@@ -22,4 +22,25 @@ s consists of lowercase English letters.
 
 class Solution:
     def removeDuplicateLetters(self, s: str) -> str:
-        pass
+        """
+        贪心 + 单调栈
+        要想使返回结果的字典序最小，则应尽量使返回结果中的字母升序，因此可使用一个单调递增栈。
+        若当前字母小于栈顶字母，且栈顶字母在之后位置还存在，则可以pop栈顶字母；若栈顶字母在之后不存在了，则此时必须保留。
+        """
+        # 记录每个字母最后一次出现的下标
+        ch2idx = {}
+        for idx, ch in enumerate(s):
+            ch2idx[ch] = idx
+        stack = []
+        chs_in_stack = set()
+        for idx, ch in enumerate(s):
+            if ch not in chs_in_stack:
+                while stack and stack[-1] > ch and ch2idx[stack[-1]] > idx:
+                    chs_in_stack.remove(stack.pop())
+                chs_in_stack.add(ch)
+                stack.append(ch)
+        return ''.join(stack)
+
+
+if __name__ == '__main__':
+    print(Solution().removeDuplicateLetters(s="cbacdcbc"))
