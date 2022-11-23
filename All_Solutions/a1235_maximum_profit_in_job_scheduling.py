c892091894fa518ef96285cc9493e5af07ca8495
Liu Quan 刘泉 <liuquan@wisers.com>
2022-11-23 14:17:08 +0800

    update

--- All_Solutions/a1235_maximum_profit_in_job_scheduling.py
+++ All_Solutions/a1235_maximum_profit_in_job_scheduling.py
@@ -33,4 +33,33 @@ from typing import List
 
 class Solution:
     def jobScheduling(self, startTime: List[int], endTime: List[int], profit: List[int]) -> int:
-        pass
+        """
+        排序 + 动态规划 + 二分查找
+        先将所有兼职工作按endTime升序
+        dp[i] 表示前i份兼职工作可以获得的最大报酬
+        状态转移方程：第i份兼职工作的报酬为 profit[i-1]
+        1、若不选择第i份兼职工作，则 dp[i] = dp[i-1]
+        2、若选择了第i份兼职工作，则 第i份工作的前一份工作的endTime必须小于等于第i份工作的startTime，即 找到所有满足条件的工作j，然后获取最大的dp[j]，最后加上profit[i-1]
+        由上可知，若选择当前工作不能获得最大报酬，则不会选择当前工作，此时的dp[i]等于dp[i-1]，因此可知dp数组是单调递增的。
+        只要找到满足小于等于当前工作startTime的最后一份工作j，它所对应的dp[j]就是最大的，可使用二分查找最后一份工作j，因为已对jobs按endTime升序
+        综上，两种情况取最大值，即 dp[i] = max(dp[i-1], dp[j] + profit[i-1])
+        初始值：dp[0] = 0，前0份兼职工作的报酬为0
+        """
+        jobs = sorted(zip(startTime, endTime, profit), key=lambda item: item[1])
+        dp = [0]
+        for i, (st, _, p) in enumerate(jobs):
+            left, right = -1, i - 1
+            # 若退出循环时，left等于-1，则表示不存在满足条件的工作j，此时的dp[j]为0，即 dp[0]
+            while left < right:
+                mid = (left + right + 1) // 2
+                if jobs[mid][1] <= st:
+                    left = mid
+                else:
+                    right = mid - 1
+            # dp数组的长度比jobs数组的长度大1
+            dp.append(max(dp[-1], dp[left + 1] + p))
+        return dp[-1]
+
+
+if __name__ == '__main__':
+    print(Solution().jobScheduling(startTime=[1, 1, 1], endTime=[2, 3, 4], profit=[5, 6, 4]))
