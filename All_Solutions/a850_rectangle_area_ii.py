92b96a4a13cc2647b5d569895e8284e1a0752c75
Liu Quan 刘泉 <liuquan@wisers.com>
2022-12-17 16:43:01 +0800

    update

--- All_Solutions/a850_rectangle_area_ii.py
+++ All_Solutions/a850_rectangle_area_ii.py
@@ -29,4 +29,39 @@ from typing import List
 
 class Solution:
     def rectangleArea(self, rectangles: List[List[int]]) -> int:
-        pass
+        """
+        扫描线 + 排序
+        可看作是一条竖线从左边界(最小横坐标)一直向右扫描，最终走到右边界(最大横坐标)。在扫描过程中，把整个大矩形看作是一列列的小矩形拼接而成的，
+        每个小矩形就是相邻两条扫描线夹着的部分。可理解为对高度进行积分
+        """
+        mod = 10 ** 9 + 7
+        xs = []
+        for (x1, _, x2, _) in rectangles:
+            xs.append(x1)
+            xs.append(x2)
+        xs.sort()
+        res = 0
+        for i in range(1, len(xs)):
+            x1, x2 = xs[i - 1], xs[i]
+            # 若相邻的两条扫描线重合，则说明当前的小矩形的面积为0，因此跳过
+            if x1 == x2:
+                continue
+            width = x2 - x1
+            # 过滤出覆盖了当前这两条扫描线的所有矩形
+            ys = [(bottom, top) for (left, bottom, right, top) in rectangles if left <= x1 and x2 <= right]
+            ys.sort()
+            # 题目已告知 0 <= yi1, yi2
+            height, y1, y2 = 0, -1, -1
+            for (bottom, top) in ys:
+                if bottom > y2:
+                    height += y2 - y1
+                    y1, y2 = bottom, top
+                elif top > y2:
+                    y2 = top
+            height += y2 - y1
+            res = (res + width * height) % mod
+        return res
+
+
+if __name__ == '__main__':
+    print(Solution().rectangleArea(rectangles=[[0, 0, 2, 2], [1, 0, 2, 3], [1, 0, 3, 1]]))
