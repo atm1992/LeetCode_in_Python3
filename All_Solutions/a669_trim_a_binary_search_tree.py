2d63477ceab88a4e2af4ddc9a8d82b9f7dd9617a
Liu Quan 刘泉 <liuquan@wisers.com>
2022-12-19 17:12:16 +0800

    update

--- All_Solutions/a669_trim_a_binary_search_tree.py
+++ All_Solutions/a669_trim_a_binary_search_tree.py
@@ -34,4 +34,37 @@ class TreeNode:
 
 class Solution:
     def trimBST(self, root: Optional[TreeNode], low: int, high: int) -> Optional[TreeNode]:
-        pass
+        """递归。DFS"""
+        if not root:
+            return None
+        if root.val < low:
+            return self.trimBST(root.right, low, high)
+        if root.val > high:
+            return self.trimBST(root.left, low, high)
+        # 对左子树修剪完后，返回左子树的新的根节点，然后作为root.left
+        root.left = self.trimBST(root.left, low, high)
+        root.right = self.trimBST(root.right, low, high)
+        return root
+
+    def trimBST_2(self, root: Optional[TreeNode], low: int, high: int) -> Optional[TreeNode]:
+        """迭代"""
+        # 先找到符合要求的根节点
+        while root and (root.val < low or root.val > high):
+            root = root.right if root.val < low else root.left
+        if not root:
+            return None
+        # 循环修剪左子树
+        node = root
+        while node.left:
+            if node.left.val < low:
+                node.left = node.left.right
+            else:
+                node = node.left
+        # 循环修剪右子树
+        node = root
+        while node.right:
+            if node.right.val > high:
+                node.right = node.right.left
+            else:
+                node = node.right
+        return root
